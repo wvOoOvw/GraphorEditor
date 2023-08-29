2575c3dc1f5afc6dceb92e22003af4cd5a186042
developer <1735721264@qq.com>
2023-08-29 19:50:37 +0800

    x

--- react-web-ui/src-element/Basic-Audio/Edit.jsx
+++ react-web-ui/src-element/Basic-Audio/Edit.jsx
@@ -9,19 +9,19 @@ function Edit(props) {
 
   return <Grid container spacing={1}>
     <Grid item xs={12}>
-      <TextField {...sx.TextFieldSX} fullWidth autoComplete='off' label='Src' value={element.property.src} onChange={e => { element.property.element.property.src  = e.target.value; update(); update() }} />
+      <TextField {...sx.TextFieldSX} fullWidth autoComplete='off' label='Src' value={element.property.src} onChange={e => { element.property.element.property.src = e.target.value; update(); update() }} />
     </Grid>
     <Grid item xs={12} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
       <div>Controls</div>
-      <Switch checked={element.property.controls} onChange={(e) => { element.property.controls = e.target.checked; update() }} }} />
+      <Switch checked={element.property.controls} onChange={(e) => { element.property.controls = e.target.checked; update() }} />
     </Grid>
     <Grid item xs={12} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
       <div>Autoplay</div>
-      <Switch checked={element.property.autoplay} onChange={(e) => { element.property.autoplay = e.target.checked; update() }} }} />
+      <Switch checked={element.property.autoplay} onChange={(e) => { element.property.autoplay = e.target.checked; update() }} />
     </Grid>
     <Grid item xs={12} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
       <div>Loop</div>
-      <Switch checked={element.property.loop} onChange={(e) => { element.property.loop = e.target.checked; update() }} }} />
+      <Switch checked={element.property.loop} onChange={(e) => { element.property.loop = e.target.checked; update() }} />
     </Grid>
   </Grid>
 }
