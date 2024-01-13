63ce63d3952f0dbae8deed2a92aa0adba1286374
developer <1735721264@qq.com>
2024-01-13 16:13:58 +0800

    x

--- src/View.Entry.Dev.jsx
+++ src/View.Entry.Dev.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import { Grid } from '@mui/material'
+import { Dialog } from '@mui/material'
+import { DialogContent } from '@mui/material'
+
 import NavigationBar from './View.NavigationBar'
 import NavigationTabs from './View.NavigationTabs'
 import NavigationContent from './View.NavigationContent'
@@ -9,14 +13,27 @@ import graphElement from '../src-element/index'
 import Imitation from './utils.imitation'
 import { hash } from './utils.common'
 
+function ScreenDialog() {
+  return <Dialog open={true} sx={{ '& .MuiDialog-paper': { width: 'fit-content' } }}>
+    <DialogContent className='font'>
+      <Grid container spacing={2}>
+        <Grid item xs={12} className='font'>
+          Pressing the keyboard <span style={{ color: Imitation.state.theme.palette.primary.main }}>Ctrl -</span> To zoom out screen size
+        </Grid>
+      </Grid>
+    </DialogContent>
+  </Dialog>
+}
+
 function App() {
   const [visible, setVisible] = React.useState()
   const [height, setHeight] = React.useState()
+  const [screenDialog, setScreenDialog] = React.useState()
 
   const onMouseOver = () => {
     Imitation.assignState({ elementHover: undefined, elementDragEnd: undefined })
   }
-  
+
   React.useEffect(() => {
     Imitation.state.graphElement = graphElement
     Imitation.state.graphElementUpdate = hash()
@@ -41,7 +58,8 @@ function App() {
     const event = () => {
       setHeight(window.innerHeight - 32)
 
-      if (window.innerWidth < 1800 || window.innerHeight < 900) Imitation.assignState({ message: `suggest 'ctrl -' to zoom out screen` })
+      if (window.innerWidth < 1800 || window.innerHeight < 900) setScreenDialog(true)
+      if (window.innerWidth >= 1800 && window.innerHeight > 900) setScreenDialog(false)
     }
 
     event()
@@ -53,19 +71,25 @@ function App() {
 
   if (visible === undefined || height === undefined) return null
 
-  return <div style={{ width: 'calc(100% - 32px)', height: height, display: 'flex', flexDirection: 'column', padding: 16 }} onMouseOver={onMouseOver}>
-    <div style={{ marginBottom: 16 }}>
-      <NavigationBar />
-    </div>
-    <div style={{ width: '100%', height: 0, flexGrow: 1, display: 'flex' }}>
-      <div style={{ width: 'fit-content', height: '100%', marginRight: 16 }}>
-        <NavigationTabs />
+  return <>
+    <div style={{ width: 'calc(100% - 32px)', height: height, display: 'flex', flexDirection: 'column', padding: 16 }} onMouseOver={onMouseOver}>
+      <div style={{ marginBottom: 16 }}>
+        <NavigationBar />
       </div>
-      <div style={{ width: 0, height: '100%', flexGrow: 1 }}>
-        <NavigationContent />
+      <div style={{ width: '100%', height: 0, flexGrow: 1, display: 'flex' }}>
+        <div style={{ width: 'fit-content', height: '100%', marginRight: 16 }}>
+          <NavigationTabs />
+        </div>
+        <div style={{ width: 0, height: '100%', flexGrow: 1 }}>
+          <NavigationContent />
+        </div>
       </div>
     </div>
-  </div>
+
+    {
+      screenDialog ? <ScreenDialog /> : null
+    }
+  </>
 }
 
 export default App
